ed826454708934737b72bc49699b538d456edf65
AA <aa@AAs-MacBook-Air.local>
2022-05-23 17:56:05 +0300

    feat:commit

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,32 +1,45 @@
 const fs = require('fs');
 const path= require('path');
-
-fs.rmSync(path.join(__dirname,'files-copy'), { recursive: true, force: true });
-
-//создаем папку
-    fs.mkdir(path.join(__dirname,'files-copy'), (err) => {
+//удаляем папку
+(async ()=>{
+ let x=await fs.rm(path.join(__dirname,'files-copy'), {recursive: true, force: true}, (error) => {
+   if (error)
+      console.log(error);
+      console.log('удалили');
+        //создаем папку
+        fs.mkdir(path.join(__dirname,'files-copy'), (err) => {
+            console.log('создали папку');
         if (err) {
             if (!(err.errno==='-17')) {}
             else
             {return console.error(err);}
         }
-        
-    });
 
 
-//находим все файлы и копируем их
+            //находим все файлы и копируем их
 
-fs.readdir(path.join(__dirname,'files'),{ withFileTypes: true }, (err, files) => {
-    if (err)
-      console.log(err);
-    else {
-      files.forEach(file => {
-        
-            fs.copyFile(path.join(__dirname,'files',file.name), path.join(__dirname,'files-copy',file.name), (err) => {
-                if (err) {
-                    return console.error(err);
+            fs.readdir(path.join(__dirname,'files'),{ withFileTypes: true }, (err, files) => {
+                if (err)
+                console.log(err);
+                else {
+                files.forEach(file => {
+                    
+                        fs.copyFile(path.join(__dirname,'files',file.name), path.join(__dirname,'files-copy',file.name), (err) => {
+                            if (err) {
+                                return console.error(err);
+                            }
+                            });
+                })
                 }
-                });
-      })
-    }
-  })
+            });
+
+        });
+  });
+ 
+    
+})();
+
+
+
+
+
